4e1e1eb5a2d3e84b9c9bfe7c9e03d46e3f461236
kris52h6 <kris52h6@stud.kea.dk>
2024-06-10 13:27:58 +0200

    added cors for client url

--- server/index.ts
+++ server/index.ts
@@ -3,10 +3,17 @@ import "dotenv/config";
 import dbConnect from "./startup/dbConnect";
 import authRouter from "./routes/authRouter";
 import itemRouter from "./routes/itemRouter";
+import cors from "cors";
 
 dbConnect();
 
 const app = express();
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/item", itemRouter);
