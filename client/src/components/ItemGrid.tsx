09a29100ae398099728f125831437d6a302345c7
kris52h6 <kris52h6@stud.kea.dk>
2024-06-12 13:34:36 +0200

    added loading skeletons

--- client/src/components/ItemGrid.tsx
+++ client/src/components/ItemGrid.tsx
@@ -1,13 +1,12 @@
 import useItems from "@/hooks/useItems";
 import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { Link } from "react-router-dom";
+import { Skeleton } from "./ui/skeleton";
 
 function ItemGrid() {
     const { data, isLoading, error } = useItems();
+    const skeletons = [...Array(20).keys()];
 
-    if (isLoading) {
-        return <p>Loading...</p>;
-    }
     if (error) {
         return <p>Error: {error.message}</p>;
     }
@@ -15,6 +14,12 @@ function ItemGrid() {
     return (
         <>
             <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                {isLoading &&
+                    skeletons.map((skeleton) => (
+                        <div key={skeleton} className="space-y-2">
+                            <Skeleton className="h-4 h-[250px] w-[250px]" />
+                        </div>
+                    ))}
                 {data?.results?.map((item) => (
                     <Link key={item._id} to={`/items/${item._id}`}>
                         <Card className="flex flex-col justify-between h-64">
