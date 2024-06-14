7c1b3e3d6231d5d85d2ca8e40cc2bb9ca8c465bb
kris52h6 <kris52h6@stud.kea.dk>
2024-06-14 13:02:38 +0200

    invalidate query when items is added

--- client/src/hooks/useAddItem.ts
+++ client/src/hooks/useAddItem.ts
@@ -1,11 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import ApiClient from "@/services/api-client";
 
 const apiClient = new ApiClient<FormData>("/items");
 
 const useAddItem = () => {
+    const queryClient = useQueryClient();
+
     return useMutation({
         mutationFn: (data: FormData) => apiClient.post(data),
+        onSuccess: () => {
+            queryClient.invalidateQueries(["items"]);
+        },
     });
 };
 
