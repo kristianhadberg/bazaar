ac4cb429f7182baf99ed5f6451529068e4542833
kris52h6 <kris52h6@stud.kea.dk>
2024-06-14 13:14:21 +0200

    footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="h-32 bg-foreground flex justify-center">
+            <h1 className="text-xl text-background items-center leading-[100px]">Bazaar</h1>
+        </div>
+    );
+}
+
+export default Footer;
--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,12 +1,16 @@
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
 import "../App.css";
+import Footer from "@/components/Footer";
 
 const Layout = () => {
     return (
         <>
-            <NavBar />
-            <Outlet />
+            <div className="layout min-h-screen">
+                <NavBar />
+                <Outlet />
+            </div>
+            <Footer />
         </>
     );
 };
